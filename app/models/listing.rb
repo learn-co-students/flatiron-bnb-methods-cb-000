5e67c7c5eb8abdcbb072ffebe43360e1ba139eec
jwald1 <email@example.com>
2017-05-26 13:04:52 -0400

    city_spec passes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,31 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, :listing_type, :title, :price, :description, :neighborhood,
+    presence: true
+
+  after_create :set_user_as_host
+  after_destroy :detract_host_status
+
+  def self.available(start_date, end_date)
+    left_outer_joins(:reservations).
+    merge(Reservation.not_overlap(start_date, end_date))
+  end
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  private
+
+  def set_user_as_host
+    host.update(host: true)
+  end
+
+  def detract_host_status
+    if host.listings.count.zero?
+      host.update(host: false)
+    end
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,12 @@
 class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
-  has_one :review
+  has_many :review
 
+  def self.not_overlap(start_date, end_date)
+    date_range = start_date..end_date
+    
+    where.not(checkin: date_range, checkout: date_range).
+    or(where(listing_id: nil))
+  end
 end
