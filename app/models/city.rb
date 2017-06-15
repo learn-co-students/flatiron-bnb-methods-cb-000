9f342d76a1dfb1d4237c5be60c3a369ece14bbd8
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-15 15:36:36 +0000

    update bundler

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,12 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(start_date, end_date)
+    listings.collect do |listing|
+      listing.reservations.collect do |reservation|
+        reservation.checkin >= start_date && reservation.checkout >= end_date        
+      end
+    end
+  end
 
+end
