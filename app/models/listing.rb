ea5b685b953439b8d981c55daabe99362f2fb9b2
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-18 00:37:34 +0000

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,24 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates_presence_of :address, :description, :title, :listing_type, :price, :neighborhood
+
+  before_save :host_user
+  before_destroy :de_host_user
+
+  def average_review_rating
+    self.reviews.collect(&:rating).reduce(:+) / self.reviews.count.to_f
+  end
+
+  private
+  def host_user
+    self.host.update(host: true) unless self.host.host
+  end
+
+  def de_host_user
+    if self.host.listings.count <= 1
+      self.host.update(:host => false)
+    end
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,20 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates_presence_of :rating, :description
+
+  validate :after_checkout
+  validate :reservation_accepted
+
+  private
+  def after_checkout
+    if  reservation && reservation.checkout > Date.today
+      errors.add(:reservation, "You can submit your review after you checkout")
+    end
+  end
+  def reservation_accepted
+    if reservation.try(:status) != "accepted"
+      errors.add(:reservation, "You can submit your review on accepted reservation")
+    end
+  end
 end
