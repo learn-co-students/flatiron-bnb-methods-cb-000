e299f2db4631ef8ca6f0b0a634c37e6edb673b1f
Devan <devan.moylan@backyardlion.com>
2017-07-22 15:53:40 -0600

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates_presence_of :rating, :description, :reservation
+  validate :valid_timing
+
+  private
+
+  def valid_timing
+    errors.add(:review, "Invalid timing for review") unless !reservation.nil? && reservation.status == "accepted" && reservation.checkout < Time.current
+  end
+
 end
