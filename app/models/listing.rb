4d862e5c10fae54e8e6f6630c3f491902e32e4ee
Matthew <github email address>
2017-09-19 01:25:26 +0700

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,41 @@
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
+  
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+
+  after_save :host_status
+  after_destroy :host_status
+
+  validates :address,
+            :listing_type,
+            :title,
+            :description,
+            :price,
+            :neighborhood, presence: true
+
+ 
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+ 
+    
+  def host_status    
+      if host.listings.count > 0
+        host.update(host: true)
+      else
+        host.update(host: false)
+      end
+  end
   
+  
+   
 end
+
+
+
+
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,44 @@
 class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
-  has_one :review
 
+  has_one :review
+  
+
+  validates :checkin,
+            :checkout, 
+            presence: true
+
+  validate :valid_user, :checkout_before_checkin, :available
+
+  def valid_user
+  	if guest_id == listing.host_id
+  	  errors.add(:guest, "You can not book your own room!")
+  	end
+  end
+
+  def duration
+    (checkout-checkin).to_i
+  end
+
+  def total_price
+    duration * listing.price
+  end
+   
+
+  def available
+    Reservation.where(listing_id: listing_id).where.not(id: id).each do |res|
+      dates = res.checkin..res.checkout
+      if dates === self.checkin || dates.include?(self.checkout)
+        errors.add(:guest_id, "Sorry, not available for those days.")
+      end
+    end
+  end
+
+  def checkout_before_checkin
+    if checkout && checkin && checkout <= checkin
+      errors.add(:guest_id, "Sorry, checkout can not be before checkin.")
+    end
+  end
+  
 end
