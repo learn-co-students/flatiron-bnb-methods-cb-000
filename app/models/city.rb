c8bb6a14c44358f7edc8a939d7351e76a936ccb1
sjmoody <steven@beachheadmarketing.com>
2017-11-14 14:07:08 +0100

    passing all tests for listings

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,11 @@
+require 'pry'
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(start_date, end_date)
+    #given date parameters, query for listings that have these dates available
+    binding.pry
+  end
 
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,43 @@
+require 'pry'
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  validates :address, presence:true
+  validates :listing_type, presence:true
+  validates :title, presence:true
+  validates :description, presence:true
+  validates :price, presence: true
+  validates :neighborhood, presence:true
+  before_save :make_host
+  after_destroy :destroy_host
+
+
+  def average_review_rating
+    # return average of the review rating
+    self.reviews.average(:rating).to_f
+  end
+
+  private
+    def make_host
+      # binding.pry
+      user = User.find(host_id)
+      user.host = true
+      user.save
+    end
+
+
+
+    def destroy_host
+      # check if host has any listings
+      # if no listings, set to false and save
+      l = Listing.find_by 'host_id == ?', host_id
+      if !l
+        user = User.find(host_id)
+        user.host = false
+        user.save
+      end
+    end
 end
