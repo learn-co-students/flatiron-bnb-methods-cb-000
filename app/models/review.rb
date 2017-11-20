1b95ba609bbf33027b06c9a36f2517ac8ae1a2f3
sjmoody <steven@beachheadmarketing.com>
2017-11-20 12:13:17 +0100

    three user association tests remaining

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,22 @@ class Review < ActiveRecord::Base
   validates :rating, presence:true
   validates :description, presence:true
   validates :reservation, presence:true
-
+  validate :resNotPassed
   #is invalid without an associated reservation, has been accepted, and checkout has happened (FAILED - 1)
+
+
+  def resNotPassed
+    if self.reservation
+      if self.reservation.checkout.future?
+        errors.add(:review, "cannot post a review before reservation has completed")
+      end
+    end
+
+
+    end
+
+
+
+
 
 end
