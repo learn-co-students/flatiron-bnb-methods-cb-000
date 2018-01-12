454093ed8528ffab507249e2ab6194f141e3b740
Joe Corwin <joe.corwin@gmail.com>
2018-01-12 14:47:30 +0000

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,47 @@
 class Listing < ActiveRecord::Base
+  before_create :change_user_status
+  after_destroy :host_status_removed
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood_id, presence: true
+
+
+  def average_review_rating
+    ratings = []
+    self.reservations.each do |reservation|
+      ratings << reservation.review.rating
+    end
+    ratings.sum.to_f / ratings.size
+  end
+
+
+
+
+  private
+
+  def change_user_status
+    self.host.host = true
+    self.host.save
+  end
+
+  def host_status_removed
+  if Listing.all.none? {|listing| listing.host == self.host}
+    self.host.host = false
+  else
+    self.host.host = true
+  end
+  self.host.save
+  end
+
+
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,44 @@
 class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
+  belongs_to :city
   has_one :review
+
+
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :no_res_on_own_listing?, :is_listing_available?, :checkout_greater_than_checkin
+
+  def duration
+    (self.checkout-self.checkin).to_i
+  end
+
+  def total_price
+    (self.listing.price.to_i)*duration
+  end
+
+
+  private
+
+  def no_res_on_own_listing?
+    if self.listing.host_id == guest_id
+      errors.add(:reservation, "You cannot make a reservation on your own listing")
+    end
+  end
+
+  def is_listing_available?
+    self.listing.reservations.each do |reservation|
+       if self.checkin.present? && self.checkout.present? && ((self.checkin <= reservation.checkout && self.checkin >= reservation.checkin) || (self.checkout <= reservation.checkout && self.checkout >= reservation.checkin)) && (self != reservation)
+         errors.add(:checkin, "Dates Unavailable!")
+       end
+     end
+  end
+
+  def checkout_greater_than_checkin
+    if self.checkin.present? && self.checkout.present? && (self.checkout <= self.checkin)
+      errors.add(:checkin, "Check-in date must be after Check-out date")
+    end
+  end
+
 
 end
