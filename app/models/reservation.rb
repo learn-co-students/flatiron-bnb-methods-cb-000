4623565138db32491e0009e6082938e57843fafa
Dan Blondell <danblondell@gmail.com>
2018-02-25 00:18:21 -0500

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,13 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :host_isnt_guest
+
+  def host_isnt_guest
+      if host_id == guest_id
+          errors.add(:reservation, "Host can't book their own listing.")
+      end
+  end
 end
