585e27d5be553fe9da6ec1f8cb51474ac8461302
Alex Segers <a.segers.dev@gmail.com>
2018-07-11 19:39:18 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,28 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  #city_openings
 
+  def self.highest_ratio_res_to_listings
+    ratio_arr = all.map do |city|
+      listings_count = city.listings.count.to_f
+      reservations_count = city.listings.map{|l| l.reservations.count}.inject(0, :+).to_f
+      listings_count == 0 ? 0 : reservations_count / listings_count
+    end
+
+    highest_ratio_id = ratio_arr.index(ratio_arr.max) + 1
+
+    City.find(highest_ratio_id)
+  end
+
+  def self.most_res
+    res_arr = all.map.with_index{|city| [city.name, city.listings.count]}
+    City.find(most_res_id)
+
+    end
+
+    most_res_id = ratio_arr.index(ratio_arr.max) + 1
+
+    City.find(most_res_id)
+  end
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,46 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+
+  # validations:
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood
+
+  # lifecycle callbacks:
+  before_validation :set_host_to_host
+  before_destroy :unset_host_to_host
+
+  # custom attributes:
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  def available(start_date, end_date) # ('06-01-2014', '10-01-2014')
+    date_range = ( Date.parse(start_date) .. Date.parse(end_date) ).to_a
+    dates_reserved = find_reserved_dates()
+    date_range - dates_reserved
+  end
+
+
+  private
+
+  # for lifecycle callbacks:
+
+  def set_host_to_host
+    host.update(host: true) if host
+  end
+
+  def unset_host_to_host
+    host.update(host: false) if host && host.listings.count == 1
+  end
+
+  # for custom attributes
+
+    ## for custom attribute #available
+    def find_reserved_dates
+      reserved = []
+      self.reservations.each{|r| reserved.concat( (r.checkin .. r.checkout).to_a )}
+      reserved
+    end
 end
