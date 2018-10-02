03a85688fb26287686c4ebaf5ae008c66d552786
Adam Urbina <adamurbina@gmail.com>
2018-10-02 15:38:33 +0000

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,46 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :neighborhood, presence: true
+  validates :description, presence: true
+
+  before_create :change_user_host_status
+  before_destroy :change_host_status_if_no_listings
+
+  def average_review_rating
+    review_ratings = self.reviews.collect do |r|
+      r.rating
+    end
+    review_ratings.sum / review_ratings.size.to_f
+  end
+
+  def is_available?(date)
+    self.reservations.each do |res|
+      if date >= res.checkin && date <= res.checkout
+        return false
+      end
+    end
+    true
+  end
+
+  private
+
+  def change_user_host_status
+    self.host.host = true
+    self.host.save
+  end
+
+  def change_host_status_if_no_listings
+      if self.host.listings.count == 1
+        self.host.host = false
+        self.host.save
+      end
+  end
+
+
 end
