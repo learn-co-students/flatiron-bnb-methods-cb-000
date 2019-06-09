a2b2b87cd318b47f82cc6e4344f62c71b67333db
Laura Felce <crepu83@yahoo.it>
2019-06-09 15:23:50 +0000

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,31 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  before_create :change_user_status
+  before_destroy :clear_host_status
+
+
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
+
+
+  def average_review_rating
+    ratings = []
+      self.reservations.each do |res|
+      ratings << res.review.rating
+   end
+
+   ratings.sum.to_f / ratings.size
+  end
+
+  def change_user_status
+    self.host.update(host: true)
+  end
+
+  def clear_host_status
+    if self.host.listings.count <= 1
+      host.update(host: false)
+    end
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,22 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+
+  validates_presence_of :rating, :description, :reservation
+  validate :existing_res, :checkout
+
+
+  def existing_res
+    if reservation.try(:status) != 'accepted'
+      errors.add(:reservation, "You need an existing reservation to leave a review.")
+    end
+  end
+
+  def checkout
+    if reservation && reservation.checkout > Date.today
+      errors.add(:reservation, "Checkout has to happended to leave a review")
+    end
+  end
+
+
 end
